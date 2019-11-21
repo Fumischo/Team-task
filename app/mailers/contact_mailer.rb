3b1a0d3c7df8b95453674f0490cefbe7c36b148d
fumischo <nil.admirari1009@gmail.com>
2019-11-21 18:08:06 +0900

    add the file to convey the right to other ppl

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -4,12 +4,12 @@ class ContactMailer < ApplicationMailer
     def contact_mail(email, team)
         @email = email
         @team = team
-        mail to: @email, subject: "#{@team.name}のリーダー権を付与されました！"
+        mail to: @email, subject: "#{@team.name}のリーダー権が譲渡されました。"
     end
 
     def agenda_delete_mail(email, title)
       @email = email
       @title = title
-      mail to: @email, subject: "#{title}が削除されました！"
+      mail to: @email, subject: "#{title}が削除されました。"
     end
   end
